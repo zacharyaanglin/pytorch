ba82271c8f29cc7da9c196fb77102dc08596acfd
Jason Ansel <jansel@meta.com>
2023-04-12 08:09:14 -0700

    Fix torch.compile() on a skipped module
    ghstack-source-id: eeca1f913518937e96400cf6ab1b050050866a9c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/98894

--- torch/_dynamo/external_utils.py
+++ torch/_dynamo/external_utils.py
@@ -1,5 +1,32 @@
 # This module contains functions that *will be allowed* by dynamo
 
+import functools
+
+import torch.nn
+
 
 def is_compiling():
     return False
+
+
+def wrap_inline(fn):
+    """
+    Create an extra frame around fn that is not in skipfiles
+    """
+    if isinstance(fn, torch.nn.Module):
+        return WrapperModule(fn)
+
+    @functools.wraps(fn)
+    def inner(*args, **kwargs):
+        return fn(*args, **kwargs)
+
+    return inner
+
+
+class WrapperModule(torch.nn.Module):
+    def __init__(self, mod):
+        super().__init__()
+        self.mod = mod
+
+    def __call__(self, *args, **kwargs):
+        return self.mod(*args, **kwargs)
